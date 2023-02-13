1ef7fba18e81a4657595cdc1295ce43a75376c6b
Daniel Garcia-Briseno <dg.briseno@gmail.com>
2023-02-13 00:47:33 -0500

    Add error message for no product info

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
 pub mod brocade;
 pub mod error;
 use std::error::Error;
-use brocade::Brocade;
+use crate::brocade::Brocade;
 
 use crate::error::ResponseError;
 #[cfg(feature = "reqwest" )]
 pub fn instance() -> Brocade {
     fn getter(url: &str) -> Result<String, Box<dyn Error>> {
         let result = reqwest::blocking::get(url)?;
-        match result.text() {
-            Ok(data) => Ok(data),
-            Err(_) => Err(Box::new(ResponseError::new("An error occurred")))
+        if result.status().is_success() {
+            match result.text() {
+                Ok(data) => Ok(data),
+                Err(_) => Err(Box::new(ResponseError::new("An error occurred")))
+            }
+        } else {
+            return Err(Box::from(ResponseError::new(format!("No product information found").as_str())));
         }
     }
     Brocade::new(getter)
@@ -22,8 +26,15 @@ mod test {
 
     #[test]
     fn test_reqwest() {
-        let api = reqwest();
+        let api = instance();
         let result = api.get("00076950450233");
+        assert_eq!(result.unwrap().brand_name.unwrap(), "Yogi");
+    }
+
+    #[test]
+    fn test_no_product() {
+        let api = instance();
+        let result = api.get("00076950450232");
         dbg!(result);
     }
 }
